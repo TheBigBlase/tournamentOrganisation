766ff3e1908c1ee86ab2ea9fc45077aa64f69ce6
TheDiscordDude <damien.dasseux@etu.univ-smb.fr>
2022-05-23 11:08:42 +0200

    corrected error : display wrong result

--- src/views/signinup.php
+++ src/views/signinup.php
@@ -1,81 +1,105 @@
 <?php
-	include('./header.php');
-    // Inscription
-	if(isset($_POST['user_firstname']) && $_POST['user_firstname'] != '' && 
-	isset($_POST['user_lastname']) && $_POST['user_lastname'] != '' && 
-	isset($_POST['user_mail']) && $_POST['user_mail'] != '' && 
-	isset($_POST['user_mdp']) && $_POST['user_mdp'] != '')
-    {
-        $sql = "INSERT INTO user(firstname, lastname, mail, password, idUT) VALUES(?, ?, ?, ?, 3)";
-        $req = $conn->prepare($sql);
-        $req->bind_param("ssss", $_POST['user_firstname'], $_POST['user_lastname'], $_POST['user_mail'], $_POST['user_mdp']);
-				try {
-					$req->execute();
-				}
-				catch(Exception $e){
-					die("<h3 color=red> Erreur : ".$e->getMessage());
-				}
+/** @var $conn mysqli */
+include('./header.php');
+var_dump($_SESSION);
+// Inscription
+if(!empty($_POST['user_firstname']) &&
+    !empty($_POST['user_lastname']) &&
+    !empty($_POST['user_mail']) &&
+    !empty($_POST['user_mdp']) &&
+    !empty($_POST['user_mdp_confirm'])
+)
+{
+    $ok = true;
 
-        $req->close();
-        $conn->close();
-				echo "<h3 color=green> Vous avez bien été enregistré. </h3>";
+    if($_POST['user_mdp'] != $_POST['user_mdp_confirm']){
+        $ok = false;
     }
 
-    // Connexion
-
-    elseif(isset($_POST['user_mail_c']) && $_POST['user_mail_c'] != '' && isset($_POST['user_mdp_c']) && $_POST['user_mail_c'] != '')
-    {
-        $sql = "SELECT * FROM user WHERE mail = ? AND password = ?";
-        $req = $conn->prepare($sql);
-        $req->bind_param("ss",$_POST['user_mail_c'], $_POST['user_mdp_c']);
+    $sql = "INSERT INTO user(firstname, lastname, mail, password, idUT) VALUES(?, ?, ?, ?, 3)";
+    $req = $conn->prepare($sql);
+    $req->bind_param("ssss", $_POST['user_firstname'], $_POST['user_lastname'], $_POST['user_mail'], $_POST['user_mdp']);
+    try {
         $req->execute();
+    }
+    catch(Exception $e){
+        die("<h3 style='color:red'> Error : ".$e->getMessage());
+    }
 
-        $res = $req->get_result();
-        $data = $res->fetch_assoc();
-        
+    $req->close();
+    $conn->close();
+    echo "<h3 style='color:green'> You have now been registered </h3>";
+}
+
+// Connection
+
+elseif(!empty($_POST['user_mail_c']) && !empty($_POST['user_mdp_c']))
+{
+    $sql = "
+            SELECT u.firstname, u.lastname, u.userId, uT.UTname
+            FROM user u join userType uT on uT.idUT = u.idUT
+            WHERE mail = ? AND password = ?
+            
+        ";
+    $req = $conn->prepare($sql);
+    $req->bind_param("ss",$_POST['user_mail_c'], $_POST['user_mdp_c']);
+    $req->execute();
+
+    $res = $req->get_result();
+    $data = $res->fetch_assoc();
+    if(empty($data)){
+        echo "<p style='color:red'>Wrong mail / password</p>";
+    }
+    else{
         $_SESSION['ID'] = $data['userId'];
         $_SESSION['name'] = $data['firstname'] . " " . $data['lastname'];
+        $_SESSION['type'] = $data["UTname"];
 
         echo $_SESSION['name'];
 
         $req->close();
         $conn->close();
-				echo "<h3 color=green> Vous etes connecté. </h3>";
+        echo "<h3 style='color:green'> You are now connected </h3>";
     }
-	
+}
 ?>
 
-<h2>S'inscrire</h2>
+<h2>Sign up</h2>
 <form action="signinup.php" method="post">
     <div>
-        <label for="prénom">prénom :</label>
-        <input type="text" id="prenom" name="user_firstname">
+        <label for="user_firstname">Firstname :</label>
+        <input type="text" id="user_firstname" name="user_firstname">
     </div>
     <div>
-        <label for="nom">nom :</label>
-        <input type="text" id="nom" name="user_lastname">
+        <label for="user_lastname">Lastname :</label>
+        <input type="text" id="user_lastname" name="user_lastname">
     </div>
     <div>
-        <label for="mail">e-mail&nbsp;:</label>
-        <input type="email" id="mail" name="user_mail">
+        <label for="user_mail">e-mail&nbsp;:</label>
+        <input type="email" id="user_mail" name="user_mail">
     </div>
     <div>
-        <label for="mdp">Mot de passe :</label>
-        <input id="mdp" name="user_mdp"></input>
+        <label for="user_mdp">Password :</label>
+        <input type="password" id="user_mdp" name="user_mdp">
     </div>
-    <input type="submit" value="Envoyer le formulaire">
+
+    <div>
+        <label for="user_mdp_confirm">Confirm password :</label>
+        <input type="password" id="user_mdp_confirm" name="user_mdp_confirm">
+    </div>
+    <input type="submit" value="Sign up">
 </form>
 
-<h2>Se connecter</h2>
+<h2>Sign in</h2>
 <form action="signinup.php" method="post">
     <div>
-        <label for="mail">e-mail&nbsp;:</label>
+        <label for="mail">e-mail :</label>
         <input type="email" id="mail" name="user_mail_c">
     </div>
     <div>
-        <label for="mdp">Mot de passe :</label>
-        <input id="mdp" name="user_mdp_c"></input>
+        <label for="mdp">Password :</label>
+        <input id="mdp" name="user_mdp_c">
     </div>
 
-    <input type="submit" value="Envoyer le formulaire">
+    <input type="submit" value="Sign in">
 </form>
